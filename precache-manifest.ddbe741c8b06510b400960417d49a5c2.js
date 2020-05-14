61d8cfef2313d75acc3b8a89b3aef4f3d19971c0
janiedelhi <janiec@aplaceformom.com>
2020-05-14 13:07:32 -0700

    Updates

---
+++ precache-manifest.ddbe741c8b06510b400960417d49a5c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "258f33dba89b0b8b3f8e7e337eca2f9a",
+    "url": "/candle-store/index.html"
+  },
+  {
+    "revision": "45ef1f34e35238274d47",
+    "url": "/candle-store/static/css/main.60422efd.chunk.css"
+  },
+  {
+    "revision": "bae7a691ebaa75c26b05",
+    "url": "/candle-store/static/js/2.e3d96475.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/candle-store/static/js/2.e3d96475.chunk.js.LICENSE"
+  },
+  {
+    "revision": "45ef1f34e35238274d47",
+    "url": "/candle-store/static/js/main.5609626b.chunk.js"
+  },
+  {
+    "revision": "41cae62c6890f0754be6",
+    "url": "/candle-store/static/js/runtime-main.3651b1ab.js"
+  }
+]);
